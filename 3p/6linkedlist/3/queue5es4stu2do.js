9512ac5330c4ebbba2a6b02d805f9dab63420fd0
LuisAnt2002 <90060101+LuisAnt2002@users.noreply.github.com>
2021-11-09 14:24:17 -0600

    Create queue5es4stu2do.js

---
+++ 3p/6linkedlist/3/queue5es4stu2do.js
@@ -0,0 +1,15 @@
+function Queue(n) {
+    //props
+    this.linked = new Linked()    
+
+    //methods
+    this.enqueue = enqueue
+    this.dequeue = dequeue
+    this.front = front
+    //this.back = back
+    this.toString = toString
+    this.empty = empty
+}
+
+//implement
+function enqueue(){}
